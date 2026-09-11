9ca566ee1d3d7473db08dd2f0dd1d56e60b42ed0
raghav <rairpsharma@gmail.com>
2026-09-11 22:41:22 +0530

    Saving files

---
+++ Basics/Operations.js
@@ -0,0 +1,20 @@
+let value = 3;
+let negativeValue = -value;
+console.log(negativeValue);
+
+let str1 = "Raghav";
+let str2 = "Sharma";
+
+let str3 = str1 +" "+ str2;
+console.log(str3);
+
+// "+" is used for conversion
+console.log(+true);
+
+console.log(+"");
+
+// prefix operator :- modify then use.
+// postfix operator :- use then modify.
+
+
+ 
