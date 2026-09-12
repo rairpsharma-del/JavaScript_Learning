8bc498137f0b6546950ac8b966d38f989bd25535
raghav <rairpsharma@gmail.com>
2026-09-13 02:57:46 +0530

    Saving File

---
+++ Basics/DataTypesSummary.js
@@ -0,0 +1,39 @@
+/*
+    primitive data types:-
+    7 types:-String, Number, Boolean, null, Undefined, Symbol, BigInt
+
+    when manupilating the data/refrence variable directly the old one gets thrown out 
+    and new one gets stored in the memory.
+
+    When passing it into a function or manupilating a copy of the data the copy of the data gets changed
+    not the exact value.
+
+    that's why it is called call by value.
+
+*/
+
+/*
+    Non-Primitive data types:-
+    Array, objects, function
+
+    when you copy a non-primitive data type then both the variables ends up hold the exact same memory location.
+
+    when you pass the refrence into function the function gets a copy of the memory address.
+*/
+
+/*
+    Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
