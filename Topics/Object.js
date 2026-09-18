787de62d6c923d1dbae309028402be3a17d846a6
raghav <rairpsharma@gmail.com>
2026-09-19 00:33:15 +0530

    Study about basics of object in javascript.

---
+++ Topics/Object.js
@@ -0,0 +1,35 @@
+/*
+    Object Literal :- A Comma separated list of key-value pairs wrapped in curly braces ("{}") used to create and initializ an object directly.
+    When objects are created using constructor method the singleton is created.
+*/
+const mysym = Symbol("Key1")
+const JSuser = {
+    name:"Raghav",
+    [mysym]:"Value1",
+    age:21,
+    location:"Bihar",
+    email:"Raghav@gmail.com",
+    "isLoggedIn":true,
+    lastLoginDays:["Monday","Tuesday"]
+}
+
+// Ways to access data stored in object.
+console.log(JSuser.name);
+console.log(JSuser["email"]);
+// Only way of accessing the data of object is the key is declared as String.
+console.log(JSuser["isLoggedIn"]);
+// Only of accessing the data of the object if the data type of key is Symbol.
+console.log(JSuser["mysym"]);
+
+// Overwrite the data of the object.
+JSuser.email = "RaghavSharma@gmail.com";
+// freezes the object so that no change can be made in the object.
+// Object.freeze(JSuser);
+// The Changes stated below will not be prpagated as the object is freezed.
+JSuser.email = "Gojo@gmail.com"
+
+JSuser.greeting = function(){
+    console.log("Hello Js User");
+}
+// returns the refrence of the fucntion.
+console.log(JSuser.greeting);
